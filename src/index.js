fd1735243396a2d129b6919d25027d9535b6568c
Efim Shikanov <darkcoderb@gmail.com>
2022-08-08 23:19:23 +0500

    deleted redux-toolkit and react-redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from "react";
-import store from "./store";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
 import App from "./App";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
 
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
